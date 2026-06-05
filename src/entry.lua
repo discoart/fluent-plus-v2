df5b873631e597c3e67da7914c6870084ab8c642
discoart <discoart@users.noreply.github.com>
2026-06-05 22:54:54 +0300

    fix: make bundler work, remove fisch bypass and telemetry

--- src/entry.lua
+++ src/entry.lua
@@ -1,1 +1,7 @@
-print('mr penis')
+local Fluent, SaveManager, InterfaceManager, Mobile = require("init")
+
+_G.Fluent = Fluent
+_G.SaveManager = SaveManager
+_G.InterfaceManager = InterfaceManager
+
+return Fluent, SaveManager, InterfaceManager, Mobile
